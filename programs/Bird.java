f5f8b8f8eca25b23eba7f586424d098f87b81a78
_patil_28_ <142707200+ANITAPATIL2814@users.noreply.github.com>
2024-08-17 18:00:00 +0530

    Add files via upload

---
+++ programs/Bird.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class Bird extends Animal{
+public void makeSound() {
+		System.out.println(" bird chirps.");
+	}
+	public void AnimalType() {
+		System.out.println("A flying bird.");
+	}
+	public void Food() {
+		System.out.println("Bird eats seeds.");
+	}
+	public void Shelter() {
+		System.out.println("Bird lives in a nest.");
+	}
+}
